4d3988b2379de7a0ca5575f58f5940e372cf390e
Davi-html <davifernandes3674@gmail.com>
2024-01-18 18:43:03 -0300

    ajustando o tamanho dos botoes dos cards (aplicando grid)

--- src/page/styles.ts
+++ src/page/styles.ts
@@ -116,13 +116,11 @@ export const Container = styled.div<ContainerProps>`
                 
                 justify-content: space-around;
 
-                display: flex;
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                gap: 20px;
 
                 margin-top: 10px;
-                
-                button {
-                    padding: 0 10px;
-                }
             }
 
             background: #2C243B;
